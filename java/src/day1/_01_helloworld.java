bb57332591a02587a5e4d7180017ac229cd83c42
th3y0xn <seoyeon061203@naver.com>
2025-02-04 16:15:31 +0900

    day1 test

---
+++ java/src/day1/_01_helloworld.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class _01_helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("안녕하세요");
+		
+	}
+
+}
